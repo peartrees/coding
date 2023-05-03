f9a4c3a5b7681978057c33cf70247870aef57203
peartrees <eggsandwich.petr@gmail.com>
2023-05-04 00:03:02 +0900

    update files

--- ABC/ABC280/A/answer.py
+++ ABC/ABC280/A/answer.py
@@ -1,2 +1,12 @@
 H, W = map(int, input().split())
-S = [input() for i in range(H)] 
+S = [input() for i in range(H)]
+
+cnt = 0
+
+for i in range(H):
+    for j in range(W):
+        if S[i][j] == "#":
+            cnt+=1
+print(cnt)
+
+
